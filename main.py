5b8d8eafb72011168f2b4340b6c6612139f98a48
PandaNinjas <admin@malwarefight.wip.la>
2023-05-28 00:25:22 -0400

    Fix bugs, use threading

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, time, pypresence, random, sys, subprocess, tty, termios
+import os, time, pypresence, random, sys, subprocess, tty, termios, threading
 from typing import Any
 from pygame import mixer
 
@@ -10,15 +10,15 @@ except ImportError:
 
 class MusicPlayer:
     presence: "pypresence.Presence | None"
-    songs: "list[str]"
     normal_tty_settings: "list[Any]"
     diff: "float | None"
     playing: "bool"
+    presence_update_lock: "threading.Lock"
 
-    def __init__(self, presence, songs, initial):
+    def __init__(self, presence, initial):
         self.presence = presence
-        self.songs = songs
         self.normal_tty_settings = termios.tcgetattr(sys.stdin.fileno())
+        self.presence_update_lock = threading.Lock()
         self.setraw()
         mixer.init()
         if initial is not None:
@@ -27,7 +27,7 @@ class MusicPlayer:
 
     def run(self):
         while True:
-            self.play(random.choice(self.songs))
+            self.play(random.choice(os.listdir(".")))
 
     def setraw(self):
         tty.setraw(sys.stdin.fileno())
@@ -40,24 +40,30 @@ class MusicPlayer:
 
     def update(self, *args, **kwargs):
         if self.presence != None:
-            self.presence.update(*args, **kwargs, buttons=[{"label": "Source code for this music player", "url": "https://github.com/pandaninjas/Inflo"}])
+            self.presence.update(*args, **kwargs, buttons=[{"label": "Source code", "url": "https://github.com/pandaninjas/Inflo"}])
+
+    def reload_presence(self, name, end):
+        self.presence = pypresence.Presence("1033827079994753064")
+        self.presence.connect()
+        self.presence.update(state=f"Listening to {name}", end=end)
 
     def play(self, song: str) -> None:
         name = song.replace(".mp3", "").strip()
-        end = time.time() + self.get_length(song)
+        length = self.get_length(song)
+        end = time.time() + length
         mixer.music.load(song)
         mixer.music.play()
         self.playing = True
         self.update(state=f"Listening to {name}", end=end)
+        last_time_update = time.time()
         while mixer.music.get_busy() or not self.playing:
             c = self.getch()
             if c == "s":
                 print()
                 return
             elif c == "r":
-                self.presence = pypresence.Presence("1033827079994753064")
-                self.presence.connect()
-                self.presence.update(state=f"Listening to {name}", end=end)
+                with self.presence_update_lock:
+                    threading.Thread(target=self.reload_presence, args=(name, end)).start()                                
             elif c == "p":
                 if self.playing:
                     self.diff = end - time.time()
@@ -82,9 +88,14 @@ class MusicPlayer:
                         state=f"Listening to {name}", end=time.time() + self.diff
                     )
                     end = time.time() + self.diff
+            
+            end = (length - mixer.music.get_pos() / 1000) + time.time()
             self.unsetraw()
             # x1b for ESC
             if self.playing:
+                if last_time_update + 10 <= time.time():
+                    last_time_update = time.time()
+                    self.update(state=f"Listening to {name}", end=end)
                 print(
                     f"\x1b[2K\r\x1b[1A\x1b[2K\rNow playing {name}, time left: {(end - time.time()):.2f}\ncontrols: [s]kip, [r]eload presence, [p]ause",
                     end="",
@@ -134,6 +145,4 @@ except Exception as e:
     print("No pres: " + str(e))
     pres = None
 
-songs = list(filter(lambda k: k.endswith("mp3"), os.listdir()))
-
-player = MusicPlayer(pres, songs, sys.argv[1] if len(sys.argv) > 1 else None)
+player = MusicPlayer(pres, sys.argv[1] if len(sys.argv) > 1 else None)
