0808d4842107c78ff123514f55121054b0f4aa4f
PandaNinjas <admin@malwarefight.wip.la>
2023-10-18 18:23:43 -0700

    Bar for song progress

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, time, pypresence, random, sys, subprocess, threading, json, argparse, atexit, re, requests, requests_cache, math, unicodedata
+import os, time, pypresence, random, sys, subprocess, threading, json, argparse, atexit, re, requests, requests_cache, math, unicodedata, datetime
 
 try:
     import tty, termios
@@ -33,7 +33,7 @@ MOVE_AND_CLEAR_LINE = "\x1b[1A\x1b[2K"
 class MusicPlayer:
     presence: "pypresence.Presence | None"
     normal_tty_settings: "list[Any]"
-    diff: "float | None"
+    diff: "tuple[float, float] | None"
     playing: "bool"
     presence_update_lock: "threading.Lock"
     weights_file: "str"
@@ -57,6 +57,8 @@ class MusicPlayer:
             atexit.register(self.unsetraw)
         self.setraw()
         mixer.init()
+        if self.presence != None:
+            atexit.register(self.presence.close)
         if self.initial is not None:
             self.play(self.initial)
         self.run()
@@ -98,63 +100,64 @@ class MusicPlayer:
             )
 
     def update(self, *args, **kwargs):
-        buttons = [
-            {
-                "label": "Source code",
-                "url": "https://github.com/pandaninjas/Inflo",
-            }
-        ]
-        match = YOUTUBE_DL_ID_REGEX.search(kwargs["state"])
-        large_image_url = None
-        channel_name = None
-        if match:
-            video_id = match.group(0).strip("[]")
-            if self.disable_api:
-                large_image_url = (
-                    f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
-                )
-                channel_name = None
-            else:
-                try:
-                    api_result = requests.get(
-                        "https://inflo-api.thefightagainstmalware.workers.dev/"
-                        + video_id,
-                        timeout=1
-                    ).json()
-                    large_image_url = api_result["maxres"]
-                    channel_name = api_result["channelTitle"]
-                except Exception:
+        with self.presence_update_lock:
+            buttons = [
+                {
+                    "label": "Source code",
+                    "url": "https://github.com/pandaninjas/Inflo",
+                }
+            ]
+            match = YOUTUBE_DL_ID_REGEX.search(kwargs["state"])
+            large_image_url = None
+            channel_name = None
+            if match:
+                video_id = match.group(0).strip("[]")
+                if self.disable_api:
                     large_image_url = (
                         f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
                     )
                     channel_name = None
-            if "end" in kwargs:
-                buttons.append(
-                    {
-                        "label": "Join",
-                        "url": f"https://youtube.com/watch?v={video_id}&t={round(self.length - (kwargs['end'] - time.time()))}",
-                    }
-                )
-            else:
-                buttons.append(
-                    {"label": "Join", "url": f"https://youtube.com/watch?v={video_id}"}
-                )
-
-        if self.presence != None:
-            try:
-                self.presence.update(
-                    *args,
-                    **kwargs,
-                    large_image=large_image_url,
-                    buttons=buttons,
-                    large_text=channel_name,
-                )
-            except Exception:
+                else:
+                    try:
+                        api_result = requests.get(
+                            "https://inflo-api.thefightagainstmalware.workers.dev/"
+                            + video_id,
+                            timeout=1
+                        ).json()
+                        large_image_url = api_result["maxres"]
+                        channel_name = api_result["channelTitle"]
+                    except Exception:
+                        large_image_url = (
+                            f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
+                        )
+                        channel_name = None
+                if "end" in kwargs:
+                    buttons.append(
+                        {
+                            "label": "Join",
+                            "url": f"https://youtube.com/watch?v={video_id}&t={round(self.length - (kwargs['end'] - time.time()))}",
+                        }
+                    )
+                else:
+                    buttons.append(
+                        {"label": "Join", "url": f"https://youtube.com/watch?v={video_id}"}
+                    )
+            if self.presence != None:
                 try:
-                    self.presence.close()
+                    self.presence.update(
+                        *args,
+                        **kwargs,
+                        large_image=large_image_url,
+                        buttons=buttons,
+                        large_text=channel_name,
+                        instance=False,
+                    )
                 except Exception:
-                    pass
-                self.presence = None
+                    try:
+                        self.presence.close()
+                    except Exception:
+                        pass
+                    self.presence = None
 
     def reload_presence(self, name, end):
         try:
@@ -165,12 +168,23 @@ class MusicPlayer:
         self.presence.connect()
         self.update(state=name, end=end)
 
+    def render_progress_bar(self, start: float, end: float) -> str:
+        cur_time = time.time()
+        bar_width = os.get_terminal_size().columns - 14
+        left_bar_width = int((cur_time - start) / (end - start) * bar_width)
+        print(left_bar_width)
+        right_bar_width = bar_width - left_bar_width
+        mins_start, secs_start = divmod(cur_time - start, 60)
+        mins_end, secs_end = divmod(end - cur_time, 60)
+        return f"{int(mins_start)}:{int(secs_start):02d} \033[32m{left_bar_width * '━'}\033[0m{right_bar_width * '━'} -{int(mins_end)}:{int(secs_end):02d}"
+
     def play(self, song: str) -> None:
         name = song.replace(".mp3", "").strip()
         self.length = self.get_length(song)
+        start = time.time()
         end = time.time() + self.length
         self.playing = True
-        self.update(state=name, end=end)
+        self.update(state=name, start=start, end=end)
         count = 0
         mixer.music.load(song)
         mixer.music.play()
@@ -186,7 +200,7 @@ class MusicPlayer:
                     ).start()
             elif c == "p":
                 if self.playing:
-                    self.diff = end - time.time()
+                    self.diff = (time.time() - start, end - time.time())
                     mixer.music.pause()
                     self.playing = False
                     if self.presence is not None:
@@ -197,7 +211,7 @@ class MusicPlayer:
                         )
                     self.unsetraw()
                     print(
-                        f"\x1b[2K\r{MOVE_AND_CLEAR_LINE * (self.lines_written - 1)}\rPaused: {name}, time left: {self.diff:.2f}\ncontrols: [s]kip, [r]eload presence, [p]lay, volume [u]p, volume [d]own\nvolume: {self.volume:.2f}",
+                        f"\x1b[2K\r{MOVE_AND_CLEAR_LINE * (self.lines_written - 1)}\rPaused: {name}\n{self.render_progress_bar(start, end)}\ncontrols: [s]kip, [r]eload presence, [p]lay, volume [u]p, volume [d]own\nvolume: {self.volume:.2f}",
                         end="",
                     )
                     self.setraw()
@@ -206,22 +220,26 @@ class MusicPlayer:
                         assert self.diff is not None
                     mixer.music.unpause()
                     self.playing = True
-                    self.update(state=name, end=time.time() + self.diff)
-                    end = time.time() + self.diff
+                    end = time.time() + self.diff[1]
+                    start = time.time() - self.diff[0]
+                    self.update(state=name, start=start, end=end)
             elif c == "u":
                 self.volume += 0.01
+                self.volume = min(1, self.volume)
                 mixer.music.set_volume(self.volume)
             elif c == "d":
                 self.volume -= 0.01
+                self.volume = max(0, self.volume)
                 mixer.music.set_volume(self.volume)
             if count % 100 == 0:
                 end = (self.length - mixer.music.get_pos() / 1000) + time.time()
+                start = time.time() - (mixer.music.get_pos() / 1000)
             # x1b for ESC
             if self.playing:
                 self.unsetraw()
                 if count % 100 == 0:
-                    self.update(state=name, end=end)
-                to_print = f"Now playing {name}, time left: {(end - time.time()):.2f}\ncontrols: [s]kip, [r]eload presence, [p]ause, volume [u]p, volume [d]own\nvolume: {self.volume:.2f}"
+                    self.update(state=name, start=start, end=end)
+                to_print = f"Now playing {name}\n{self.render_progress_bar(start, end)}\ncontrols: [s]kip, [r]eload presence, [p]ause, volume [u]p, volume [d]own\nvolume: {self.volume:.2f}"
                 print(
                     f"\x1b[2K\r{MOVE_AND_CLEAR_LINE * (self.lines_written - 1)}{to_print}\r",
                     end="",
@@ -231,13 +249,8 @@ class MusicPlayer:
             count += 1
             time.sleep(0.01)
         print(
-            f"\x1b[2K\r{MOVE_AND_CLEAR_LINE * (self.lines_written - 1)}\rNow playing {name}, time left: 0.00\ncontrols: [s]kip, [r]eload presence, [p]ause, volume [u]p, volume [d]own\nvolume: {self.volume:.2f}",
-            end="",
+            f"\x1b[2K\r{MOVE_AND_CLEAR_LINE * (self.lines_written - 1)}Now playing {name}\n{self.render_progress_bar(start, time.time())}\ncontrols: [s]kip, [r]eload presence, [p]ause, volume [u]p, volume [d]own\nvolume: {self.volume:.2f}",
         )
-        print(
-            "\n" * (self.lines_written - 3), end=""
-        )
-        print("\x1b")
 
     def term_length(self, string: str) -> int:
         length = 0
